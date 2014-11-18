ff31940fef74aceb168a71ae87fea3d94cf29d2d
y4smeen <y4smeen@gmail.com>
2014-11-18 09:06:25 -0500

    ok

---
+++ TwoD/TwoD.java
@@ -0,0 +1,19 @@
+public class TwoD {
+    public static void main(String[] args) {
+	int[][] a;
+	a = new int[5][3];
+	System.out.println(a.length);
+	System.out.println(a[0].length);
+
+	a[4][2]=2;
+	a[1][2]=9;
+
+	for (int i = 0; i < a.length; i++) {
+	    for (int j = 0; j < a[i].length; j++) {
+		System.out.println(a[i][j]+" ");
+	    }
+	    System.out.println();
+
+	}
+    }
+}
